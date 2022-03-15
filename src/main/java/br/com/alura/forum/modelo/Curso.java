cab326963de898fe1021d070983bb28303160477
leonardo.rodrigues <leobazacas@gmail.com>
2022-03-15 16:17:02 -0300

    Restaurant CRUD done

---
+++ src/main/java/br/com/alura/forum/modelo/Curso.java
@@ -0,0 +1,63 @@
+package br.com.alura.forum.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Curso {
+
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private String categoria;
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Curso other = (Curso) obj;
+		if (id == null) {
+			return other.id == null;
+		}
+		return id.equals(other.id);
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO USUARIO(nome, email, senha) VALUES('Voter', 'voter@email.com', '$2a$10$sFKmbxbG4ryhwPNx/l3pgOJSt.fW1z6YcUnuE2X8APA/Z3NI/oSpq');
+
+INSERT INTO RESTAURANT(name, description, votes) VALUES('RestaurantOne', 'Cheap indian restaurant', 0);
+INSERT INTO RESTAURANT(name, description, votes) VALUES('RestaurantTwo', 'Expensive japanese restaurant', 0);
+INSERT INTO RESTAURANT(name, description, votes) VALUES('RestaurantThree', 'Average BBQ option', 0);
