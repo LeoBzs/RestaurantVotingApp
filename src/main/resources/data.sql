8b2920f835a2c00a6bf4423852a87085de1a81a4
leonardo.rodrigues <leobazacas@gmail.com>
2022-03-16 19:34:00 -0300

    Method functionalities added

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Voter', 'voter@email.com', '$2a$10$sFKmbxbG4ryhwPNx/l3pgOJSt.fW1z6YcUnuE2X8APA/Z3NI/oSpq');
+INSERT INTO USUARIO(nome, email, senha, vote) VALUES('Voter', 'voter@email.com', '$2a$10$sFKmbxbG4ryhwPNx/l3pgOJSt.fW1z6YcUnuE2X8APA/Z3NI/oSpq', 1);
 
 INSERT INTO RESTAURANT(name, description, votes) VALUES('RestaurantOne', 'Cheap indian restaurant', 0);
 INSERT INTO RESTAURANT(name, description, votes) VALUES('RestaurantTwo', 'Expensive japanese restaurant', 0);
